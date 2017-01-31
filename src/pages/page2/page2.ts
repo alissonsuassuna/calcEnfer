ef69a7024cabd8436820a72f6e7763c974445987
Alisson Suassuna <alissonsuassuna@gmail.com>
2017-01-31 16:20:37 -0300

    Completo BMI calculation

--- src/pages/page2/page2.ts
+++ src/pages/page2/page2.ts
@@ -8,17 +8,33 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class Page2 {
 
-   peso: number;
-   altura: number;
-   imc: number;
+   weight: number;
+   height: number;
+   imc: any;
+   name: string;
 
   constructor(public nav: NavController, public navParams: NavParams) {
-    this.altura = this.altura;
-    this.peso = this.peso;
+    this.height = this.height;
+    this.weight = this.weight;
   }
 
   calcImc():void {
-    this.imc = this.peso / (this.altura * this.altura);
+
+    this.imc = this.weight / (this.height * this.height);
+    this.imc.toFixed(2);
+    if(this.imc <= 18.5 ){
+      this.name = "Você está baixo do peso com esse IMC";
+    }else if(this.imc <= 24.9){
+      this.name = "Seu peso é ideal com esse IMC"
+    }else if(this.imc <= 29.9){
+      this.name = "Você está levemento acima do seu peso com esse IMC";
+    }else if(this.imc <= 34.9){
+      this.name = "Você está com Obesidade grau I com esse IMC"
+    }else if(this.imc <= 39.9){
+      this.name = "Você está com Obesidade grau II (Severa) com esse IMC: "
+    }else{
+      this.name = "Você está com Obesidade III (Móbida) com esse IMC: "
+    }
   }
 
   itemTapped(event, item) {
